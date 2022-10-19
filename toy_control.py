242de976904f6d32db0b5aac513ce66d5bd102fc
Joy Lunkad <joylunkad@gmail.com>
2022-10-19 17:41:47 +0530

    Update toy_control.py

--- toy_control.py
+++ toy_control.py
@@ -4,9 +4,9 @@ import wandb
 wandb.login(key="3dbab9409a8508ef6618bd9c199a164f94743b3d")
 
 # Assumes sweep is already created and initialized
-# To create run -> wandb sweep toy_sweep.yaml --project=toy
+# To create run -> wandb sweep toy_sweep.yaml --project=toy --entity='joylunkad'
 
 sweep_id = "ppggk4vv" 
 
 if __name__ == '__main__':
-    wandb.agent(sweep_id, project='toy', count=1, entity='toy_entity')
+    wandb.agent(sweep_id, project='toy', count=1, entity='joylunkad')
