517cf6f7fcdb99851e41610646323ce363edfaf3
Joy Lunkad <joylunkad@gmail.com>
2022-10-19 17:32:16 +0530

    Update toy_control.py

--- toy_control.py
+++ toy_control.py
@@ -3,11 +3,10 @@ import wandb
 # this is a key for my public account 
 wandb.login(key="3dbab9409a8508ef6618bd9c199a164f94743b3d")
 
-# Assumes finetune_sweep is already created and initialized
+# Assumes sweep is already created and initialized
 # To create run -> wandb sweep toy_sweep.yaml --project=toy
 
 sweep_id = "ppggk4vv" 
 
 if __name__ == '__main__':
-    # Create TPU and setup requirements
     wandb.agent(sweep_id, project='toy', count=1, entity='toy_entity')
