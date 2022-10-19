60588cbc180efff4fc109cee0ad812972edfa195
Joy Lunkad <joylunkad@gmail.com>
2022-10-19 17:32:10 +0530

    Update toy_startup_script.sh

--- toy_startup_script.sh
+++ toy_startup_script.sh
@@ -6,4 +6,11 @@ source /home/joylunkad/.bashrc && \
 pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html && \
 git clone https://github.com/Joy-Lunkad/toy-startup-script-error.git && \
 pip install wandb && \
+cd toy-startup-script-error && \
 python3 toy_control.py' 
+
+# gcloud compute tpus tpu-vm create node-1 --zone=us-central1-f --accelerator-type=v2-8 --version=tpu-vm-base --preemptible --metadata-from-file=startup-script=toy_startup_script.sh
+
+# gcloud compute tpus tpu-vm ssh node-1  --zone us-central1-f
+
+# cat /var/log/syslog | grep startup-script
